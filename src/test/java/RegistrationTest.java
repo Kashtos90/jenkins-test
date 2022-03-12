b48c169a143ad3416acf487f5bd567c3c3df2053
Sergei Kashtuev <29061990a@gmail.com>
2022-03-13 00:40:50 +0700

    added attachments

--- src/test/java/RegistrationTest.java
+++ src/test/java/RegistrationTest.java
@@ -1,6 +1,8 @@
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -24,6 +26,14 @@ public class RegistrationTest {
         capabilities.setCapability("enableVNC",true);
         capabilities.setCapability("enableVideo",true);
         Configuration.browserCapabilities = capabilities;
+    }
+
+    @AfterEach
+    void addAttachments () {
+        Attach.screenshotAs("Screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
     }
 
     @Test
